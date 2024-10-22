863d29e137baccf9272962e0f4844a19a21717a8
Bailang Cheng <chenglang00@gmail.com>
2024-10-22 13:29:14 +0900

    Initial Commit

---
+++ Design Tool/Design_ToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_ToolApp.swift
+//  Design Tool
+//
+//  Created by Bailang Cheng on 2024/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Design_ToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Design ToolTests/Design_ToolTests.swift
@@ -0,0 +1,17 @@
+//
+//  Design_ToolTests.swift
+//  Design ToolTests
+//
+//  Created by Bailang Cheng on 2024/10/22.
+//
+
+import Testing
+@testable import Design_Tool
+
+struct Design_ToolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
